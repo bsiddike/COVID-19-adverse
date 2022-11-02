30ed1e948001259c901cb40cffd09f1227cfaf1d
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-11-03 01:21:08 +0600

    widget optimized

--- resources/views/backend/wizard/affected-gender.blade.php
+++ resources/views/backend/wizard/affected-gender.blade.php
@@ -4,30 +4,10 @@
             <h3 class="card-title">Gender Wise Affected</h3>
         </div>
         <div class="card-body">
-            <canvas id="pieChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+            <canvas id="pieChart"
+                    style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;">
 
-{{--            <div class="row">
-                <div class="col-md-5">
-                    <ul class="chart-legend clearfix">
-                        @php $total = array_sum($pieData['datasets'][0]['data']); @endphp
-                        <li><i class="far fa-circle text-danger"></i>
-                            Female ({{ percent($total, $pieData['datasets'][0]['data'][0]) }})
-                        </li>
-                        <li><i class="far fa-circle text-success"></i>
-                            Male ({{ percent($total, $pieData['datasets'][0]['data'][1]) }})
-                        </li>
-                        <li><i class="far fa-circle text-warning"></i>
-                            Unknown ({{ percent($total, $pieData['datasets'][0]['data'][2]) }})
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-7">
-                    <div class="chart-responsive">
-                        <canvas id="pieChart" ></canvas>
-                    </div>
-                </div>
-            </div>--}}
-            <!-- /.row -->
+            </canvas>
         </div>
     </div>
 </div>
@@ -41,8 +21,8 @@
                 options: {
                     maintainAspectRatio: false,
                     responsive: true,
-                    legend : {
-                        position : 'left'
+                    legend: {
+                        position: 'left'
                     }
                 }
             });

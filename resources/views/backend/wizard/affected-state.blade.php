c7a76b66d7f239f14058fc1dbda0446b677363d3
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-11-05 00:39:00 +0600

    line chart added

--- resources/views/backend/wizard/affected-state.blade.php
+++ resources/views/backend/wizard/affected-state.blade.php
@@ -32,6 +32,66 @@
 
 @push('page-script')
     <script>
+
+        $(function () {
+            var lineChartCanvas = $('#lineChart').get(0).getContext('2d')
+            var lineChartData = $.extend(true, {}, {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [
+                    {
+                        label: 'Digital Goods',
+                        backgroundColor: 'rgba(60,141,188,0.9)',
+                        borderColor: 'rgba(60,141,188,0.8)',
+                        pointRadius: false,
+                        pointColor: '#3b8bba',
+                        pointStrokeColor: 'rgba(60,141,188,1)',
+                        pointHighlightFill: '#fff',
+                        pointHighlightStroke: 'rgba(60,141,188,1)',
+                        data: [28, 48, 40, 19, 86, 27, 90]
+                    },
+                    {
+                        label: 'Electronics',
+                        backgroundColor: 'rgba(210, 214, 222, 1)',
+                        borderColor: 'rgba(210, 214, 222, 1)',
+                        pointRadius: false,
+                        pointColor: 'rgba(210, 214, 222, 1)',
+                        pointStrokeColor: '#c1c7d1',
+                        pointHighlightFill: '#fff',
+                        pointHighlightStroke: 'rgba(220,220,220,1)',
+                        data: [65, 59, 80, 81, 56, 55, 40]
+                    },
+                ]
+            })
+            lineChartData.datasets[0].fill = false;
+            lineChartData.datasets[1].fill = false;
+
+            var lineChart = new Chart(lineChartCanvas, {
+                type: 'line',
+                data: lineChartData,
+                options: {
+                    datasetFill: false,
+                    maintainAspectRatio: false,
+                    responsive: true,
+                    legend: {
+                        display: false
+                    },
+                    scales: {
+                        xAxes: [{
+                            gridLines: {
+                                display: false,
+                            }
+                        }],
+                        yAxes: [{
+                            gridLines: {
+                                display: false,
+                            }
+                        }]
+                    }
+                }
+            });
+        })
+
+
         $(document).ready(function () {
             var doughnut = new Chart($('#affectedAgeWise').get(0).getContext('2d'), {
                 type: 'pie',

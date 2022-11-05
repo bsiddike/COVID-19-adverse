b91d99b4bbb3e59fc2c5c69577d2b9f76bad6286
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-11-06 02:50:57 +0600

    filter added

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -42,6 +42,57 @@
 
 @section('content')
     <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">Filter Options</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+
+                    </div>
+                    {!! Form::open(['route' => 'backend.dashboard', 'method'=> 'get']) !!}
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-4">
+                                {!! \Form::nText('year', 'Year', request()->get('year'), false) !!}
+                            </div>
+                            <div class="col-md-4">
+                                {!! \Form::nText('age', 'Age', request()->get('age'), false) !!}
+                            </div>
+                            <div class="col-md-4">
+                                {!! \Form::nSelect('sex', 'Sex',
+                                ['M' => 'Male', 'F' => 'Female', 'U' => 'Unknown'],
+                                 request()->get('sex'), false, [ 'placeholder' => 'Select a sex']) !!}
+                            </div>
+                            <div class="col-md-4">
+                                {!! \Form::nSelect('vaccine_id', 'Vaccine',
+                                \App\Models\Vaccine::all()->pluck('vax_name', 'vaers_id')->toArray(),
+                                 request()->get('vaccine_id'), false, [
+                                     'placeholder' => 'Select a vaccine Brand name'
+                                 ]) !!}
+                            </div>
+                            <div class="col-md-4">
+                                {!! \Form::nText('symptom', 'Symptom', request()->get('symptom'), false) !!}
+                            </div>
+                            <div class="col-md-4">
+                                {!! \Form::nText('state', 'State', request()->get('state'), false) !!}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Search</button>
+                    </div>
+                    {!! \Form::close() !!}
+                </div>
+            </div>
+        </div>
         <!-- Small boxes (Stat box) -->
         <div class="row">
             @include('backend.wizard.total-patient')

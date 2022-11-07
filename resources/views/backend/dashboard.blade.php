4542b4a6e906c65d507f999a1e3119092f294502
mah.shamim <mah.shamim@gmail.com>
2022-11-08 03:52:48 +0600

    remove vaccine search

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -72,11 +72,11 @@
                                  request()->get('sex'), false, [ 'placeholder' => 'Select a sex']) !!}
                             </div>
                             <div class="col-md-4">
-                                {!! \Form::nSelect('vax_name', 'Vaccine',
+                                {{--{!! \Form::nSelect('vax_name', 'Vaccine',
                                 \App\Models\Vaccine::all()->where('vax-type','COVID19')->groupBy('vax_name')->pluck('vax_name', 'vax_name')->toArray(),
                                  request()->get('vax_name'), false, [
                                      'placeholder' => 'Select a vaccine Brand name'
-                                 ]) !!}
+                                 ]) !!}--}}
                             </div>
                             <div class="col-md-4">
                                 {!! \Form::nText('symptom', 'Symptom', request()->get('symptom'), false) !!}

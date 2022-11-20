b0fcdbaa41a122ef9a73fb7ed8828cda9e8535db
Hafijul Islam <hafijul233@gmail.com>
2022-11-21 00:54:22 +0600

    helper updated

--- app/helpers.php
+++ app/helpers.php
@@ -2,18 +2,18 @@
 
 use Carbon\Carbon;
 
-if (! function_exists('percent')) {
+if (!function_exists('percent')) {
     function percent($total, $individual, $decimals = 2, $thou_sep = '', $symbol = '%')
     {
         return ($total != 0)
-            ? (number_format((($individual * 100) / $total), $decimals, '.', $thou_sep).$symbol)
-            : 0 .$symbol;
+            ? (number_format((($individual * 100) / $total), $decimals, '.', $thou_sep) . $symbol)
+            : 0 . $symbol;
     }
 }
 
-if (! function_exists('server_date')) {
+if (!function_exists('server_date')) {
     /**
-     * @param  string|null  $input
+     * @param string|null $input
      * @return string|null
      */
     function server_date(string $input = null): ?string
@@ -29,12 +29,12 @@ if (! function_exists('server_date')) {
     }
 }
 
-if (! function_exists('strtonumber')) {
+if (!function_exists('strtonumber')) {
     /**
      * convert a string value to float character
      *
-     * @param  string  $value
-     * @param  int  $default
+     * @param string $value
+     * @param int $default
      * @return float
      */
     function strtonumber($value, $default = 0)
@@ -47,13 +47,13 @@ if (! function_exists('strtonumber')) {
     }
 }
 
-if (! function_exists('convert_datetime')) {
+if (!function_exists('convert_datetime')) {
     /**
      * convert a datetime value to another timezone datetime
      *
      * @param $datetime
-     * @param  string  $toTimeZone
-     * @param  string  $fromTimeZone
+     * @param string $toTimeZone
+     * @param string $fromTimeZone
      * @return Carbon
      */
     function convert_datetime($datetime, string $toTimeZone = 'UTC', string $fromTimeZone = 'UTC')
@@ -62,19 +62,19 @@ if (! function_exists('convert_datetime')) {
     }
 }
 
-if (! function_exists('random_color')) {
+if (!function_exists('random_color')) {
     /**
      * @return string
      */
     function random_color(): string
     {
-        return '#'.(str_pad(dechex(mt_rand(0, 255)), 2, '0', STR_PAD_LEFT).
-                str_pad(dechex(mt_rand(0, 255)), 2, '0', STR_PAD_LEFT).
+        return '#' . (str_pad(dechex(mt_rand(0, 255)), 2, '0', STR_PAD_LEFT) .
+                str_pad(dechex(mt_rand(0, 255)), 2, '0', STR_PAD_LEFT) .
                 str_pad(dechex(mt_rand(0, 255)), 2, '0', STR_PAD_LEFT));
     }
 }
 
-if (! function_exists('clean')) {
+if (!function_exists('clean')) {
     function clean($str, $default = null)
     {
         $str = trim($str);
@@ -84,7 +84,8 @@ if (! function_exists('clean')) {
             : $default;
     }
 }
-if (! function_exists('filter')) {
+
+if (!function_exists('filter')) {
     function filter(array &$data)
     {
         foreach ($data as $key => $value) {
@@ -95,9 +96,22 @@ if (! function_exists('filter')) {
     }
 }
 
-if (! function_exists('query')) {
+if (!function_exists('query')) {
     function query($route, Illuminate\Http\Request $request)
     {
-        return route($route).'?'.http_build_query($request->query());
+        return route($route) . '?' . http_build_query($request->query());
+    }
+}
+
+if (!function_exists('data_limit')) {
+    /**
+     * @param $filename
+     * @param $divider
+     * @return bool
+     */
+    function data_limit($filename, $divider = 1)
+    {
+        $file_count = (int)filter_var($filename, FILTER_SANITIZE_NUMBER_INT);
+        return (($file_count % $divider) == 0);
     }
 }

62004e4fbcf72fac60425102399ba1327cebddfe
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2022-11-21 23:22:36 +0600

    patients table again join with is_joined check 10

--- app/Repositories/Eloquent/Backend/Organization/SymptomRepository.php
+++ app/Repositories/Eloquent/Backend/Organization/SymptomRepository.php
@@ -157,11 +157,11 @@ class SymptomRepository extends EloquentRepository
 
                 $query->groupBy($filters['search_column']);
             }
-            $query->select(['symptoms.symptom1', 'symptoms.symptom2', 'symptoms.symptom3', 'symptoms.symptom4', 'symptoms.symptom5', 'patients.other_meds'])
-                ->where(DB::raw('LENGTH(patients.other_meds)'), '>', 0)
-                ->whereIn(DB::raw('LOWER(TRIM(patients.other_meds))'), ['none', null])
-                ->whereNotNull('patients.other_meds');
         }
+        $query->select(['symptoms.symptom1', 'symptoms.symptom2', 'symptoms.symptom3', 'symptoms.symptom4', 'symptoms.symptom5', 'patients.other_meds'])
+            ->where(DB::raw('LENGTH(patients.other_meds)'), '>', 0)
+            ->whereIn(DB::raw('LOWER(TRIM(patients.other_meds))'), ['none', null])
+            ->whereNotNull('patients.other_meds');
 
         return $query;
     }

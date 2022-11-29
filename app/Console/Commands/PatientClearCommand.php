ec23bb1eab9614104cb644c8e9057934c2e36d17
Hafijul Islam <hafijul233@gmail.com>
2022-11-30 03:12:08 +0600

    test commadn added

---
+++ app/Console/Commands/PatientClearCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Patient;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class PatientClearCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'patient:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        try {
+            //$dates = array_unique(Patient::select('todays_date')->pluck('todays_date')->toArray());
+            $dates = ["2020-12-18"];
+
+            foreach ($dates as $date) {
+                dd(DB::table('patients')->where('todays_date', '=', $date)->limit(500)->offset(10000000)->get()->toArray());
+            }
+
+            return Command::SUCCESS;
+
+        } catch (\Exception $exception) {
+            throw  new \Exception($exception->getMessage());
+            return Command::FAILURE;
+
+        }
+    }
+}

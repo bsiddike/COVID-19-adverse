2bb9415c1f6515e852bfcf1103ce94eb60a09cf4
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2022-12-03 00:44:48 +0600

    force https add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
         Blade::directive('money', function ($string) {
             return "<?php echo \App\Supports\Utility::money($string); ?>";
         });
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
